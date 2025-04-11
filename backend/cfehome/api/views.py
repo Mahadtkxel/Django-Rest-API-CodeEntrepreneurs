3927d09e96cb56612a36a9db77e8f10c36438024
TK-LPT-0888 <muhammad.suleman@camp1.tkxel.com>
2025-04-11 14:37:53 +0500

    tried http response

--- backend/cfehome/api/views.py
+++ backend/cfehome/api/views.py
@@ -1,22 +1,25 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
+from django.forms.models import model_to_dict
 import json
+from products.models import Product
+
+
+
 
 def api_home(request, *args, **kwargs):
-    # request is not same as python library requests
-    # request -> HttpRequest (class)-> Django
-    print(request.GET)
-    print(request.POST)
-    body = request.body # byte string of JSON data
+    model_data = Product.objects.all().order_by('?').first()
     data = {}
-    try:
-        data = json.loads(body) # string of JSON data -> Python dict
-    except:
-        pass
-    print(data)
-    # data['headers'] = request.headers # old -> request.META ->
-    data['params'] = dict(request.GET)
-    data['headers'] = dict(request.headers)
-    data['content_type'] = request.content_type
 
-    return JsonResponse(data)
+    if model_data:
+        # data['id'] = model_data.id
+        # data['title'] = model_data.title
+        # data['content'] = model_data.content
+        # data['price'] = model_data.price
+        data = model_to_dict(model_data, fields=['id', 'price','title'])
+    return JsonResponse(data)    
+        # data = dict(data)
+        #json_data_str = json.dumps(data)
+        # response = HttpResponse(data, content_type="application/json")
+        # print(response['content-type'])
+    # return HttpResponse(data, content_type="application/json")
