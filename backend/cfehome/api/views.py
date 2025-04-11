f0628454d0291fd0cb32e256f2ccd2ea6beec1b1
TK-LPT-0888 <muhammad.suleman@camp1.tkxel.com>
2025-04-11 14:56:19 +0500

    completed up to 1:06:33

--- backend/cfehome/api/views.py
+++ backend/cfehome/api/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
-from django.http import JsonResponse, HttpResponse
+# from django.http import JsonResponse, HttpResponse
 from django.forms.models import model_to_dict
 import json
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 from products.models import Product
 
 
 
-
+@api_view(["GET"])
 def api_home(request, *args, **kwargs):
     model_data = Product.objects.all().order_by('?').first()
     data = {}
@@ -16,8 +19,8 @@ def api_home(request, *args, **kwargs):
         # data['title'] = model_data.title
         # data['content'] = model_data.content
         # data['price'] = model_data.price
-        data = model_to_dict(model_data, fields=['id', 'price','title'])
-    return JsonResponse(data)    
+        data = model_to_dict(model_data, fields=['id', 'price', 'sale_price','title'])
+    return Response(data)    
         # data = dict(data)
         #json_data_str = json.dumps(data)
         # response = HttpResponse(data, content_type="application/json")
