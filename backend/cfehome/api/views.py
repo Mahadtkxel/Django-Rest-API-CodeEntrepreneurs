9479e88dd866a935d6ee885dd9b4aa19b437aa65
TK-LPT-0888 <muhammad.suleman@camp1.tkxel.com>
2025-04-11 15:50:30 +0500

    completed up to 1:14:45

--- backend/cfehome/api/views.py
+++ backend/cfehome/api/views.py
@@ -6,20 +6,21 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from products.models import Product
-
+from products.serializers import ProductSerializer
 
 
 @api_view(["GET"])
 def api_home(request, *args, **kwargs):
-    model_data = Product.objects.all().order_by('?').first()
+    instance = Product.objects.all().order_by('?').first()
     data = {}
-
-    if model_data:
+    
+    if instance:
         # data['id'] = model_data.id
         # data['title'] = model_data.title
         # data['content'] = model_data.content
         # data['price'] = model_data.price
-        data = model_to_dict(model_data, fields=['id', 'price', 'sale_price','title'])
+        # data = model_to_dict(instance, fields=['id', 'price', 'sale_price','title'])
+        data = ProductSerializer(instance).data
     return Response(data)    
         # data = dict(data)
         #json_data_str = json.dumps(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -4,7 +4,7 @@ import requests
 #endpoint = "https://httpbin.org/anything"
 endpoint = "http://127.0.0.1:8000/api/"
 get_response = requests.get(endpoint , params={"abc": 123},json={"query": "Hello World"})
-print(get_response.headers)
+# print(get_response.headers)
 
 # HTTP Request -> HTML
 # REST API HTTP Request -> JSON (JavaScript Object Notation) (in other words formatted data that python can use)
