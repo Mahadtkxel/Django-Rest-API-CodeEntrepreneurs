fb409fcbf229b3d364a013d93385301d189823d2
TK-LPT-0888 <muhammad.suleman@camp1.tkxel.com>
2025-04-11 13:11:32 +0500

    completed course upto 50:54

--- backend/cfehome/api/views.py
+++ backend/cfehome/api/views.py
@@ -5,12 +5,18 @@ import json
 def api_home(request, *args, **kwargs):
     # request is not same as python library requests
     # request -> HttpRequest (class)-> Django
-    
+    print(request.GET)
+    print(request.POST)
     body = request.body # byte string of JSON data
     data = {}
     try:
         data = json.loads(body) # string of JSON data -> Python dict
     except:
         pass
-    print(data.keys())
+    print(data)
+    # data['headers'] = request.headers # old -> request.META ->
+    data['params'] = dict(request.GET)
+    data['headers'] = dict(request.headers)
+    data['content_type'] = request.content_type
+
     return JsonResponse(data)
