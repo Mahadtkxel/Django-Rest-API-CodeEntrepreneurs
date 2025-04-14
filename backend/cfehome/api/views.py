0738241b61ed23d7f11c80f211805b4e93e53a3a
TK-LPT-0888 <muhammad.suleman@camp1.tkxel.com>
2025-04-14 11:13:08 +0500

    tried post method

--- backend/cfehome/api/views.py
+++ backend/cfehome/api/views.py
@@ -9,19 +9,29 @@ from products.models import Product
 from products.serializers import ProductSerializer
 
 
-@api_view(["GET"])
+@api_view(["POST"])
 def api_home(request, *args, **kwargs):
     instance = Product.objects.all().order_by('?').first()
     data = {}
+
+    data = request.data
+    serializer = ProductSerializer(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+
+        # instance = serializer.save()
+        # print(instance)
+        print(serializer.data)
+        return Response(serializer.data)
+    return Response({"invalid": "not good data"}, status=400)
     
-    if instance:
-        # data['id'] = model_data.id
-        # data['title'] = model_data.title
-        # data['content'] = model_data.content
-        # data['price'] = model_data.price
-        # data = model_to_dict(instance, fields=['id', 'price', 'sale_price','title'])
-        data = ProductSerializer(instance).data
-    return Response(data)    
+    # if instance:
+    #     # data['id'] = model_data.id
+    #     # data['title'] = model_data.title
+    #     # data['content'] = model_data.content
+    #     # data['price'] = model_data.price
+    #     # data = model_to_dict(instance, fields=['id', 'price', 'sale_price','title'])
+    #     data = ProductSerializer(instance).data
+        
         # data = dict(data)
         #json_data_str = json.dumps(data)
         # response = HttpResponse(data, content_type="application/json")
