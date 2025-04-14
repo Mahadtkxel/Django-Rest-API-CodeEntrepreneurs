65c3e3b5dfa57c5165602e1b8fc41a558df28857
TK-LPT-0888 <muhammad.suleman@camp1.tkxel.com>
2025-04-14 11:15:38 +0500

    updated params

--- py_client/basic.py
+++ py_client/basic.py
@@ -3,7 +3,7 @@ import requests
 #endpoint = "https://httpbin.org/status/200"
 #endpoint = "https://httpbin.org/anything"
 endpoint = "http://127.0.0.1:8000/api/"
-get_response = requests.get(endpoint , params={"abc": 123},json={"query": "Hello World"})
+get_response = requests.post(endpoint , params={"abc": 123},json={"title": "abc","content": "Hello check", "price": 123})
 # print(get_response.headers)
 
 # HTTP Request -> HTML
